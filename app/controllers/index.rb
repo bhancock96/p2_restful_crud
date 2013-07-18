abbe8645df657f9d00b86324790063813343bb86
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 20:09:26 -0500

    Add to controller, add views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,19 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create' do
+
+  erb :create_note
+end
+
+post '/create' do
+  @note = Note.create(title: params[:title], content: params[:content])
+  @note.save
+  redirect '/notes' 
+end
+
+get '/notes' do
+  @notes = Note.all
+  erb :notes
+end
