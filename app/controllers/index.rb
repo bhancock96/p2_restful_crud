6a0a1b7f19ea6380b47d3d6b2cc5b764830cb54a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-20 16:38:29 -0500

    Added delete and edit buttons

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,16 +5,36 @@ end
 
 get '/create' do
 
-  erb :create_note
+  erb :_create_note
 end
 
-post '/create' do
-  @note = Note.create(title: params[:title], content: params[:content])
-  @note.save
-  redirect '/notes' 
-end
-
-get '/notes' do
+get '/notes_list' do
   @notes = Note.all
+  erb :notes
+end
+
+
+# get '/notes/:id/edit' do
+#   @note = Note.find(params[:id])
+#   erb :edit
+# end
+
+
+post '/notes' do
+  @note = Note.create(title: params[:title], content: params[:content])  
+
+  redirect "/notes_list"
+end
+
+
+post '/notes/:id' do
+  @note = Note.find(params[:id])
+  @note.update_attributes(params[:note])
+  redirect "/notes/#{@note.id}" 
+end
+
+get '/notes/:id' do
+  @notes = Note.all
+  @note = Note.find(params[:id])
   erb :notes
 end
